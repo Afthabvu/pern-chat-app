59719850d4506b3b72d3897887dd3bce95f8dda1
afthab <afthabvu@gmail.com>
2024-08-13 05:10:24 +0530

    implemented user signup controller function

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,8 +1,53 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
+import bcryptjs from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: "Please fill all the fields" });
+    }
 
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password's don't match" });
+    }
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
 
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-export const signup = async (req:Request, res:Response) => {};
-export const login = async (req:Request, res:Response) => {};
-export const logout = async (req:Request, res:Response) => {};
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+      },
+    });
+
+    if (newUser) {
+      generateToken(newUser.id, res);
+      res.status(201).json({
+        id: newUser.id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid data" });
+    }
+  } catch (error: any) {
+    console.log("Error in signup controller ", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
+export const login = async (req: Request, res: Response) => {};
+export const logout = async (req: Request, res: Response) => {};

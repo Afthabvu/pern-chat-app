7b4cadf4e4b84f5c04750b93578760bf1f1ad5cb
afthab <afthabvu@gmail.com>
2024-08-13 05:24:10 +0530

    implemented user login controller function

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -49,5 +49,33 @@ export const signup = async (req: Request, res: Response) => {
     res.status(500).json({ error: "internal server error" });
   }
 };
-export const login = async (req: Request, res: Response) => {};
+
+
+
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid username" });
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+    generateToken(user.id, res);
+
+    res.status(200).json({
+      id: user.id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error:any) {
+    console.log("Error in login controller ", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
 export const logout = async (req: Request, res: Response) => {};

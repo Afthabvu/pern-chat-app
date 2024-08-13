0e73ec2c30d42019798280e89fcabe0c2bdb034c
afthab <afthabvu@gmail.com>
2024-08-13 14:53:10 +0530

    implemented getUserForSidebar in the controller function

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUserForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
+router.get("/conversations", protectRoute, getUserForSidebar);
+router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
-router.get("/:id",protectRoute,getMessages)
 
 export default router;

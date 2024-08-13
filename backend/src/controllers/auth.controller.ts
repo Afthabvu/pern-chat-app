b84e2d579c1be0e053af6e2039e28b14be2e8199
afthab <afthabvu@gmail.com>
2024-08-13 06:35:10 +0530

    implemented protectRoute middleware to authenticate user and implemented getMe controller function

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -86,7 +86,21 @@ export const logout = async (req: Request, res: Response) => {
   }
 };
 
+export const getMe = async (req: Request, res: Response) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id: req.user.id } });
+    if(!user){
+        return res.status(404).json({error:"User not found"})
+    }
+    res.status(200).json({
+        id: user.id,
+        fullName: user.fullName,
+        username: user.username,
+        profilePic: user.profilePic,
+      });
 
-export const getMe=async(req:Request,res:Response)=>{
-    
-}
+  } catch (error: any) {
+    console.log("Error in getMe controller ", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
+
+app.use(cookieParser())
 
 app.use(express.json());
 

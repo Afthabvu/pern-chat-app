a8c83ab50cdea9e4d2b1a24a463cd3a99725be9c
afthab <afthabvu@gmail.com>
2024-08-13 14:41:05 +0530

    implemented getMessages controller function

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -54,3 +54,32 @@ export const sendMessage = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user.id;
+
+    const conversation = await prisma.conversation.findFirst({
+      where: {
+        participantIds: {
+          hasEvery: [senderId, userToChatId],
+        },
+      },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+      },
+    });
+    if (!conversation) {
+      return res.status(200).json({ messages: [] });
+    }
+    res.status(200).json(conversation.messages);
+  } catch (error: any) {
+    console.log("Error in getMessage", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};

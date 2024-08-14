f9f36932d00b36898715f0717c4f9ee98f52fad6
afthab <afthabvu@gmail.com>
2024-08-14 21:16:47 +0530

    Added Zustand store for managing conversation and message states

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,17 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(5000, () => {
-  console.log("Server running on port 5000!");
+app.listen(PORT, () => {
+  console.log(`Server is running on port  ${PORT}`);
 });

f84b3534f906f6f80c79c1f33fdc89be79597858
afthab <afthabvu@gmail.com>
2024-08-15 04:24:27 +0530

    implemented SearchInput functionality

--- frontend/src/components/sidebar/SearchInput.tsx
+++ frontend/src/components/sidebar/SearchInput.tsx
@@ -1,12 +1,40 @@
 import { Search } from "lucide-react";
+import { useState } from "react";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
+import useConversation from "../../zustand/useConversation";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { conversations } = useGetConversations();
+  const { setSelectedConversation } = useConversation();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search term must be  atleast  3 characters long");
+    }
+    console.log(search);
+    const conversation = conversations.find((c: ConversationType) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+    console.log(conversation);
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error("No such user");
+    }
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search…"
         className="input-sm md:input input-bordered rounded-full sm:rounded-full w-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button
         type="submit"

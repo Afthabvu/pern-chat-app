e73344ac1323a16d41f9806bdfe7a0c850e0ba4a
afthab <afthabvu@gmail.com>
2024-08-15 06:51:41 +0530

    configuration for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
@@ -7,8 +8,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-
-
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 
@@ -16,6 +16,13 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV!=="development"){
+  app.use(express.static(path.join(__dirname,"/frontend/dist")))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+  })
+}
 server.listen(PORT, () => {
-  console.log(`Server is running on port  ${PORT}`);
+  // console.log(`Server is running on port  ${PORT}`);
 });
--- frontend/src/components/messages/MessageContainer.tsx
+++ frontend/src/components/messages/MessageContainer.tsx
@@ -8,6 +8,21 @@ const MessageContainer = () => {
   const { selectedConversation } = useConversation();
   return (
     <div className="w-full flex flex-col">
+      {!selectedConversation?(
+        <NoChatSelected/>
+      ):(
+        <>
+        {/* Header */}
+        <div className="bg-slate-500 px-4 py-2 mb-2">
+          <span className="label-text">To:</span>{" "}
+          <span className="text-gray-900 font-bold">{selectedConversation?.fullName}</span>
+        </div>
+
+        <Messages />
+        <MessageInput />
+      </>
+
+      )}
       <>
         {/* Header */}
         <div className="bg-slate-500 px-4 py-2 mb-2">
